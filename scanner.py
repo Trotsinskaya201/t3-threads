fa5394c96329c0c9702b4aa0ad61ff60501bddca
Trorsinskaya201 <yas15902840@gmail.com>
2025-05-06 11:00:00 +0300

    th3 scan0 init

--- scanner.py
+++ scanner.py
@@ -3,12 +3,12 @@ from threading import Thread
 
 N = 2**16 - 1
 
-for port in range(1,100):
+for port in range(N):
     sock = socket.socket()
     try:
         print(port)
         sock.connect(('127.0.0.1', port))
-        print("Порт", i, "открыт")
+        print("Порт", port, "открыт")
     except:
         continue
     finally:
